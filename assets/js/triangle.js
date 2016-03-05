645753c88a811c129a774d5899bfad60abf1edae
Nathan Brown <nkabrown@gmail.com>
2016-03-04 23:01:20 -0500

    ES6 variable naming and arrow functions

--- assets/js/triangle.js
+++ assets/js/triangle.js
@@ -1,14 +1,14 @@
 function buildGrid(data) {
-  var margin = {top: 10,right: 10,bottom: 10, left: 10},
+  const margin = {top: 10,right: 10,bottom: 10, left: 10},
       width = 100 - margin.right - margin.left,
       height = 100 - margin.top - margin.bottom;
 
-  var xScale = d3.scale.linear()
+  const xScale = d3.scale.linear()
       .domain([0, 100])
       .range([0, width]);
 
-  var yScale = d3.scale.linear()
-      .domain([0, d3.max(data, function(d) { return d.value; })])
+  const yScale = d3.scale.linear()
+      .domain([0, d3.max(data, (d) => d.value)])
       .range([height, 0]);
 
   d3.select('.triangle-grid')
@@ -32,17 +32,13 @@ function buildGrid(data) {
       .attr('class', 'triangles') 
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
       .append('path')
-      .attr('d', function(d) {
-        return 'M ' + xScale(5) + ' ' + yScale(0) + ' L ' + xScale(88) + ' ' + yScale(5) + ' L ' + xScale(88) + ' ' + yScale(18815) + ' Z';
-      })
+      .attr('d', () => 'M ' + xScale(5) + ' ' + yScale(0) + ' L ' + xScale(88) + ' ' + yScale(5) + ' L ' + xScale(88) + ' ' + yScale(18815) + ' Z')
       .style('fill', 'rgb(222,222,222)');
 
   d3.selectAll('.triangles')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
       .append('path')
-      .attr('d', function(d) {
-        return 'M ' + xScale(5) + ' ' + yScale(0) + ' L ' + xScale(88) + ' ' + yScale(5) + ' L ' + xScale(88) + ' ' + yScale(d.value) + ' Z'; 
-      })
+      .attr('d', (d) =>  'M ' + xScale(5) + ' ' + yScale(0) + ' L ' + xScale(88) + ' ' + yScale(5) + ' L ' + xScale(88) + ' ' + yScale(d.value) + ' Z')
       .style('fill', 'rgba(181,227,147,0.5)');
      
 }
